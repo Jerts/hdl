ac4bd1c8957e6da6bb615004b599ad647e1ca0d2
Luis Trejo <luistrejohi@gmail.com>
2021-05-22 21:58:45 -0500

    Tarea13

---
+++ Tarea13/sum_mult2.v
@@ -0,0 +1,23 @@
+module sum_mult2 (
+    input [3:0] a,b,c,d,
+    input clk,
+    output reg [7:0] salida
+);
+    reg [3:0] A,B,C,D;
+    reg [7:0] mult1,mult2 ;
+    reg [7:0] suma;
+
+    always @(posedge clk) begin
+        A<=a;
+        B<=b;
+        C<=c;
+        D<=d;
+
+        mult1<=A*B;    
+        mult2<=C*D;
+
+        suma=mult1+mult2;
+
+        salida<=suma;
+    end 
+endmodule
---
+++ Tarea13/sum_mult_conIP.v
@@ -0,0 +1,15 @@
+module sum_mult_conIP (
+    input [3:0] a,b,c,d,
+    input clk,
+    output [7:0] salida
+);
+    sum_mult_IP_noReg u1(
+        .dataa_0(a),
+        .dataa_1(b),
+        .datab_0(c),
+        .datab_1(d),
+        .clock0(clk),
+        .result(salida)
+    );
+    
+endmodule
